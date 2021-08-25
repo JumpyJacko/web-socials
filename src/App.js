1e8d70db32bdfef57bfbb5c5e3560eac31fae473
JumpyJacko <bobjacklee27@gmail.com>
2021-08-26 09:21:00 +1000

    Changed app.css and app.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import Bird from './components/bird';
 
 function App() {
-  return (
-    <div className="App">
-        <header className="App-header">
-            <Bird />            
-            <div className="footer">
-                Powered by Heroku, Written with React.js
-            </div>
-        </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Bird />            
+                <div className="footer">
+                    Powered by Heroku, Written with React.js
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
