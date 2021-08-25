231a3e1524c849eb2b6de16dd7b4c9c825a2b283
JumpyJacko <bobjacklee27@gmail.com>
2021-08-26 08:34:01 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Bird from './components/bird';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <Bird />            
+            <div className="footer">
+                Powered by Heroku, Written with React.js
+            </div>
+        </header>
     </div>
   );
 }
---
+++ src/components/bird.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from '../birb.png';
+import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+class Bird extends Component {
+    state = { socialsOpen: false }
+    toggleChildSocials = () => {
+        this.setState({ socialsOpen: !this.state.socialsOpen })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="Bird-button" onClick={this.toggleChildSocials}>
+                    <img src={logo} className="Bird-logo" alt="logo" />
+                </button>
+                <SocialsGithub socialsOpen={this.state.socialsOpen}/> 
+            </div>
+        );
+    };
+}
+
+class SocialsGithub extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false
+        }
+    }
+
+    componentDidUpdate(props) {
+        if (this.props.socialsOpen !== this.state.open) {
+            this.setState({ open: props.socialsOpen })
+        }
+    }
+
+    toggleSocials() {
+        this.setState({
+            open: true
+        })
+    }
+
+    render() {
+        return (
+            <div> 
+                <a href="https://twitter.com/JumpyJacko" className={"twitter"+this.handleClasses()}><FontAwesomeIcon icon={faTwitter} size="lg"/></a>
+                <a href="https://github.com/JumpyJacko" className={"github"+this.handleClasses()}><FontAwesomeIcon icon={faGithub} size="lg"/></a>
+                <div className="about">
+                    Hey, I'm Jackson Ly (JumpyJacko) and I'm currently learning to code.
+                </div>
+            </div>
+        );
+    };
+
+    handleClasses() {
+        if (this.state.open === true) {
+            let classes = "is_revealing";
+            return classes;
+        } else {
+            let classes = "socials";
+            return classes;
+        }
+    };
+}
+
+export default Bird;
